e4ec4be3be88e059d7609917da5e605c436c4507
santi <santiagomza12@gmail.com>
2023-04-08 19:19:48 -0300

    commit Estructura cambiada,Resposive,Agrello de boton de datos y cambio de las variables a ingles

--- main.py
+++ main.py
@@ -5,45 +5,65 @@ import button
 import mesa
 from pygame.locals import *
 
-pygame.init()
-window = pygame.display.set_mode((800, 800)) #settear el tamaño de la ventana
-pygame.display.set_caption("PoolGame") #nombre que va a tener la ventana
+#Funcion para cargar imagenes
+def imageLoad(name):
+    return pygame.image.load(name)
+
+#Funcion para hacer resposive los botones
+def resposiveHitbox(object,pos):
+    object.x = (window.get_width() - 800) / 2 + pos
+    object.hitbox = pygame.Rect(object.x, object.y, object.width, object.height)
+
+#Variables de la Resolucion
+base_width = 800
+base_height = 800
+
+#Condicion para cambiar de pantalla
+main = True
+menu = False
+start = False
+data = False
 
 posX = 0
 posY = 0
 
-#CARGADO DE IMAGENES
 
-#PANTALLA PRINCIPAL
-pantalla_principal = pygame.image.load("PLAY.png") #cargado de imagen en una variable
-pantalla_principal_activa = pygame.image.load("PLAY (ACTIVE).png")
-pantalla_principal_menu_active = pygame.image.load("MENU (ACTIVE).png")
-pantalla_principal_datos = pygame.image.load("PLAY DATOS.png")
+#Color Blanco
+white_color = (255,255,255)
 
-#PANTALLA DATOS
-pantalla_datos = pygame.image.load("DATOS (PAGINA).png")
-pantalla_datos_activa = pygame.image.load("DATOS (PAG ACTIVA).png")
+#Iniciar Pygame, hacer la ventana como una variable con la resolucion y poner el nombre del juego
+pygame.init()
+window = pygame.display.set_mode((base_width, base_height), pygame.RESIZABLE)
+pygame.display.set_caption("PoolGame")
+clock = pygame.time.Clock()
 
-#para mover una imagen...
+#Cargando Imagenes
 
-velocidad = 200 #variable para el movimiento con teclas
-blanco=(255,255,255) #variable que da el color blanco
+#Pantalla Principal
+game = imageLoad("PLAY.png")
+game_start_click = imageLoad("PLAY (ACTIVE).png")
+game_menu_click = imageLoad("MENU (ACTIVE).png")
+game_data_click = imageLoad("PLAY DATOS.png")
 
-derecha= False
-izquierda = False
-arriba = False
-abajo = False
+#Pantalla de Datos
+data_image = imageLoad("DATOS (PAGINA).png")
+data_return = imageLoad("DATOS (PAG ACTIVA).png")
 
-#rectangulo_hitbox = pygame.Rect(231,568, 575-231, 675-568) #hitbox del rectángulo de 'play' en la pantalla principal
+"""
+Esto no se usa todabia y capaz no se use
 
-#uwu
+right = False
+left = False
+up = False
+down = False
+"""
 #BOTONES PANTALLA PRINCIPAL
 start_hitbox = button.Button(231,568, 575-231, 675-568)
 menu_hitbox = button.Button(300, 696, 500 -300, 759 -696)
-datos_hitbox = button.Button(714, 706, 778-714, 782- 706)
+data_hitbox = button.Button(714, 706, 778-714, 782 - 706)
 
 #BOTÓN BACK EN DATOS
-back_datos_hitbox = button.Button(12, 18, 165-12, 64-18)
+data_return_hitbox = button.Button(12, 18, 165-12, 64-18)
 
 #SECCIÓN DE MÚSICA
 pygame.mixer.music.load('easy-lifestyle-137766.mp3') #agregar música
@@ -52,112 +72,100 @@ pygame.mixer.music.play(-1) #reproducir música en bucle
 
 pygame.mixer.music.set_volume(0.1) #setear volumen
 
-window.blit(pantalla_principal, (posX, posY))
-
 #hacer cada línea individualmente, para poder rodear la imagen con la hitbox
 topLine = mesa.MesaObject(100, 50, 500, 10)
 leftLine = mesa.MesaObject(100, 50, 10, 650)
 botLine = mesa.MesaObject(100, 700, 500, 10)
 rightLine = mesa.MesaObject(600, 50, 10, 660)
-
-
-menu = False
-start = False #condicion para mostrar la pantalla principal o el 'juego'
-principal = True
-datos = False
-
+"""
 print(f"Hitbox de la línea superior: {topLine.hitbox}")
 print(f"Hitbox de la línea izquierda: {leftLine.hitbox}")
 print(f"Hitbox de la línea inferior: {botLine.hitbox}")
 print(f"Hitbox de la línea derecha: {rightLine.hitbox}")
+"""
+window.fill(white_color)
 
 
 while True:
+    # mouse_x, mouse_y = pygame.mouse.get_pos()  # posición cartesiana del mouse
 
-    mouse_x,mouse_y = pygame.mouse.get_pos() #posición cartesiana del mouse
-    #JUEGO
-    if(start == True):
-        #dibujamos la hitbox
-        #Prueba rojo
-        window.fill(blanco)
-        leftLine.draw(window)
-        botLine.draw(window)
-        rightLine.draw(window)
-        topLine.draw(window)
-        pygame.mixer.music.stop()  # detener la música
-        #window.blit(mesa_pool_, (posX, posY))
 
-    #VENTANA MENU
-    if(menu == True):
-        window.fill(blanco)
-        pygame.mixer.music.stop()
+    #Este for revisa cada evento posible
+    for event in pygame.event.get():
 
-    #VENTANA DATOS
-    if (datos == True):
-        window.blit(pantalla_datos, (posX, posY))
-        pygame.mixer.music.stop()
+        #Revisa si la resolucion varia para ajustase
+        if event.type == pygame.VIDEORESIZE:
+            for hitbox, value in zip([start_hitbox, menu_hitbox, data_hitbox, data_return_hitbox], [231, 300, 714, 12]):
+                resposiveHitbox(hitbox, value)
+                window.fill(white_color)
+                posX = (window.get_width() - 800) / 2
+                posY = (window.get_height() - 800) / 2
 
-        #No sé por qué funciona solo con down, pero si le pongo up se rompe, funciona perfecto de todas formas
-        if back_datos_hitbox.down(event) == True:
-            window.blit(pantalla_datos_activa, (posX, posY))
-            datos = False
+        #VENTANA DE JUEGO
+        if start == True:
+            # dibujamos la hitbox
+            window.fill(white_color)
+            leftLine.draw(window)
+            botLine.draw(window)
+            rightLine.draw(window)
+            topLine.draw(window)
+            pygame.mixer.music.stop()  # detener la música
+            # window.blit(mesa_pool_, (posX, posY))
 
-    for event in pygame.event.get(): #event.get() son los eventos prefabricados de pygame
-        #1 = izquierdo 2 = rueda 3 = derecho
-        #and ((mouse_x >= 229 and mouse_y >= 568) and (mouse_x >= 567 and mouse_y >= 658))
+        # VENTANA MENU
+        if menu == True:
+            window.fill(white_color)
+            pygame.mixer.music.stop()
 
-        #Animacion de Botones
-        #START
-        if start == False and menu == False and datos == False and start_hitbox.down(event) == True:
-            window.blit(pantalla_principal_activa, (posX, posY))
+        # VENTANA DATOS
+        if data == True:
+            pygame.mixer.music.stop()
+            # No sé por qué funciona solo con down, pero si le pongo up se rompe, funciona perfecto de todas formas
+            if data_return_hitbox.down(event) == True:
+                window.blit(data_return, (posX, posY))
+            else:
+                window.blit(data_image, (posX, posY))
+            if data_return_hitbox.up(event) == True:
+                data = False
 
-        #MENU
-        elif start == False and menu == False and datos == False and menu_hitbox.down(event) == True:
+        # Animacion de Botones
+        # START
+        if start == menu == data == False and start_hitbox.down(event) == True:
+            window.blit(game_start_click, (posX, posY))
+        # MENU
+        elif start == menu == data == False and menu_hitbox.down(event) == True:
+            window.blit(game_menu_click, (posX, posY))
+        # DATOS
+        elif start == menu == data == False and data_hitbox.down(event) == True:
+            window.blit(game_data_click, (posX, posY))
+        # Pantalla principal
+        elif start == menu == data == False:
+            window.blit(game, (posX, posY))
 
-            window.blit(pantalla_principal_menu_active, (posX, posY))
-        #DATOS
-        elif start == False and menu == False and datos == False and datos_hitbox.down(event) == True:
-            window.blit(pantalla_principal_datos, (posX,posY))
-
-        #Pantalla principal
-        elif (start == False and menu == False and datos == False):
-            window.blit(pantalla_principal, (posX, posY))
-
-        #Abriendo otras pantallas
-        if start == False and menu == False and start_hitbox.up(event) == True:
+        # Abriendo otras pantallas
+        if start == menu == False and start_hitbox.up(event) == True:
             start = True
-        if start == False and menu == False and menu_hitbox.up(event) == True:
+        if start == menu == False and menu_hitbox.up(event) == True:
             menu = True
-        if start == False and menu == False and datos_hitbox.up(event) == True:
-            datos = True
+        if start == menu == False and data_hitbox.up(event) == True:
+            data = True
 
-        #DATOS
-       #if(start == False and menu == False):
+        '''
+            #Detectar colisiones (hay que probarlo con las bochas creadas)
+            if leftLine.check_collision('bocha'):
+                print("Colisión con la línea izquierda")
+            elif botLine.check_collision('bocha'):
+                print('Colisión con la línea inferior')
+            elif rightLine.check_collision('bocha'):
+                print('Colisión con la línea derecha')
+            elif topLine.check_collision('bocha'):
+                print('Colisión con la línea superior')
+            '''
 
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
 
-
-    '''
-    #Detectar colisiones (hay que probarlo con las bochas creadas)
-    if leftLine.check_collision('bocha'):
-        print("Colisión con la línea izquierda")
-    elif botLine.check_collision('bocha'):
-        print('Colisión con la línea inferior')
-    elif rightLine.check_collision('bocha'):
-        print('Colisión con la línea derecha')
-    elif topLine.check_collision('bocha'):
-        print('Colisión con la línea superior')
-    '''
-
-    #Mover imágenes con el mouse
-
-
-    '''
-    posX, posY = (pygame.mouse.get_pos())
-    posX = posX-100
-    posY = posY-100
-    '''
-    print(pygame.mouse.get_pos())
-    pygame.display.update()
+    #print(pygame.mouse.get_pos())
+    clock.tick(60)
+    pygame.display.flip()
